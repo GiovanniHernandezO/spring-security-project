1a888820137dfab971a4a2f0a5ebca8f7f6245cf
GiovanniHernandezO <giovanni.hernandez.olivares@gmail.com>
2023-10-23 13:05:47 -0300

    commit inicial

---
+++ src/test/java/com/cursos/springsecutirycourse/SpringSecutiryCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursos.springsecutirycourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecutiryCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
